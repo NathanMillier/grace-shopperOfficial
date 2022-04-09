3d58f4f6f519bc4dd9975ff10af50a00cb2ab456
Caden <Cpchambers01@gmail.com>
2022-04-09 09:07:41 -0400

    commits

--- db/orders.js
+++ db/orders.js
@@ -1,25 +1,46 @@
 const client = require("./");
 
 const getAllOrders = async () => {
-  const res = await client.query(`
+  try {
+    const res = await client.query(`
     SELECT * FROM orders;
     `);
-  return res.rows;
+    return res.rows;
+  } catch (error) {
+    throw error;
+  }
 };
 
 // const getAllOrdersById= ();
-
+const getAllOrdersById = async (Orderid) => {
+  try {
+    const res = await client.query(
+      `
+      SELECT * FROM orders WHERE id = 1$
+      `,
+      [id]
+    );
+    return res.rows;
+  } catch (error) {
+    throw error;
+  }
+};
 
 // const updateOrder();
 
 // const createOrder();
-
+const createOrder = async () => {
+  const res = await client.query(`
+    INSERT INTO ORDER
+    `);
+  return res.rows;
+};
 
 // const updateOrder();
-
 
 // const deleteOrder();
 
 module.exports = {
   getAllOrders,
+  getAllOrdersById,
 };
