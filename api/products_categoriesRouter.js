c9bfe0622260523e19c97f5138662fc90d26e42a
wells <highlands202@gmail.com>
2022-04-19 19:58:13 -0400

    started on productSingleView

--- api/products_categoriesRouter.js
+++ api/products_categoriesRouter.js
@@ -1,10 +1,9 @@
-//working on this one tomorrow (4/13/2022) additional review needed -Wells
-
 const express = require("express");
 const {
   getProductsByCategory,
   addProductToCategory,
 } = require("../db/products_categories");
+const categoriesRouter = require("./categoriesRouter");
 const { getAllCategories } = require("./categoriesRouter");
 
 const products_categoriesRouter = express.Router();
@@ -18,22 +17,6 @@ products_categoriesRouter.get("/", async (req, res) => {
   const categories = await getAllCategories;
 });
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-products_categoriesRouter.delete("/", async (req, res) => {
-    const 
-})
-=======
->>>>>>> 51ba120aabcf115b5c0c824109419611438543e2
-
 products_categoriesRouter.delete("/", async (req, res) => {});
-module.exports = products_categoriesRouter;
-<<<<<<< HEAD
-=======
-products_categoriesRouter.delete("/", async (req, res) => {});
->>>>>>> c0fff46606701ee6ebc848ed67ccdadb83d95599
-=======
 
-
-
->>>>>>> 51ba120aabcf115b5c0c824109419611438543e2
+module.exports = categoriesRouter;
--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { useState } from "react";
 import { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
@@ -41,6 +42,12 @@ const App = () => {
               <Products products={products} fetchProducts={fetchProducts} />
             }
             path="/Products"
+          />
+        </Routes>
+        <Routes>
+          <Route
+            element={<productSingleView fetchProducts={fetchProducts} />}
+            path="/productSingleView"
           />
         </Routes>
       </div>
