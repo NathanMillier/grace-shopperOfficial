88b875424f7abdec16ec7605639f4fbf73ef0fb4
Adam Perez <amperez0208@gmail.com>
2022-04-28 14:13:17 -0400

    added logo to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import { SearchOutlined, ShoppingCartOutlined } from "@material-ui/icons";
 import React from "react";
 import { Link } from "react-router-dom";
-import feetheattransparant from "./feetheattransparent.png";
+import logo from "./logo.PNG";
 
 const Navbar = ({ user, setToken, setUser, token }) => {
   return (
@@ -15,8 +15,7 @@ const Navbar = ({ user, setToken, setUser, token }) => {
           </div>
         </div>
         <Link to="/" className="navcenter">
-          FEET HEAT
-          {/* <img src={logo} alt="Feet Heat Logo" /> */}
+          <img src={logo} alt="Feet Heat Logo" />
         </Link>
         <div className="navright">
           <div className="menuCont">
