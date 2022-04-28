06e58917cafdf805fca7e3b18a5b2d5e5b0d98bc
Adam Perez <amperez0208@gmail.com>
2022-04-28 18:15:50 -0400

    admin users now display nice

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Slider from "../components/Slider";
+import Newsletter from "../components/Newsletter";
 
 const Home = ({ user }) => {
   if (!user) {
     return (
       <div>
         <Slider />
+        <Newsletter />
       </div>
     );
   }
@@ -13,6 +15,7 @@ const Home = ({ user }) => {
     <div>
       <p>Welcome back, {user.email}!</p>
       <Slider />
+      <Newsletter />
     </div>
   );
 };
