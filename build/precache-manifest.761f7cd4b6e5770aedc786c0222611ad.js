440dbfebaac0b37cf1d6e80d28fed7672e317c4c
Adam Perez <amperez0208@gmail.com>
2022-04-28 22:00:38 -0400

    script update

---
+++ build/precache-manifest.761f7cd4b6e5770aedc786c0222611ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "58cabcef0c6cdee796bb",
+    "url": "/static/js/main.58cabcef.chunk.js"
+  },
+  {
+    "revision": "502e9a5578e2c4bd86e5",
+    "url": "/static/js/1.502e9a55.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "168d6cdc54ce26f2c91c55b98cbe5309",
+    "url": "/static/media/logo.168d6cdc.PNG"
+  },
+  {
+    "revision": "70f81bbac334279a0c5a7ec53ad6b293",
+    "url": "/index.html"
+  }
+];
